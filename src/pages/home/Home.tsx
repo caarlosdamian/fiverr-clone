1496ae5dfd37271e232690e90f4b7a678fc01a27
caarlosdamian <dapecarlos@gmail.com>
2023-02-26 13:36:12 -0600

    feat(components): added new component

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Featured } from '../../components/featured/Featured'
+import { TrustedBy } from '../../components/trustedBy/TrustedBy'
 import './Home.scss'
 
 export const Home = () => {
   return (
     <div>
       <Featured/>
+      <TrustedBy/>
     </div>
   )
 }
