a3c9e86d9e7b63f2cfff3d9134a4a6c2299ab649
caarlosdamian <dapecarlos@gmail.com>
2023-02-26 13:21:15 -0600

    feat(featured): added new featured section

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Featured } from '../../components/featured/Featured'
 import './Home.scss'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Featured/>
+    </div>
   )
 }
