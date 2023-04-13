f78a5b2db5dead530f18945be6bc5ec77c51ea4e
caarlosdamian <dapecarlos@gmail.com>
2023-04-13 12:30:46 -0600

    added gig page

--- src/pages/gigs/Gigs.tsx
+++ src/pages/gigs/Gigs.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
-import './Gigs.scss'
+import React from "react";
+import "./Gigs.scss";
 
 export const Gigs = () => {
   return (
-    <div>Gigs</div>
-  )
-}
+    <div className="gigs">
+      <div className="container">
+        <span className="breadcrubs">FIVERR GRAPICHS & DESINGS</span>
+        <h1>IA Artist</h1>
+        <p>
+          Explore the boundaries of art and technology with Fiverr's AI artists
+        </p>
+        <div className="menu">
+          <div className="left">
+            <span>Budged</span>
+            <input type="text" placeholder="min" />
+            <input type="text" placeholder="max" />
+            <button>Apply</button>
+          </div>
+          <div className="right"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
