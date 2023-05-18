7fb22d3d15f392f35a038e6d34da238d3ced5308
caarlosdamian <dapecarlos@gmail.com>
2023-05-17 18:21:36 -0600

    feat(gig): added individual gig

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,14 +2,10 @@ import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import "./Navbar.scss";
 
-export const Navbar = () => {
+function Navbar() {
   const [active, setActive] = useState(false);
   const [open, setOpen] = useState(false);
-  const [currentUser, setcurrentUser] = useState({
-    id: 1,
-    username: "John",
-    isSeller: true,
-  });
+
   const { pathname } = useLocation();
 
   const isActive = () => {
@@ -18,98 +14,109 @@ export const Navbar = () => {
 
   useEffect(() => {
     window.addEventListener("scroll", isActive);
-    console.log("enter");
     return () => {
       window.removeEventListener("scroll", isActive);
     };
   }, []);
 
+  // const currentUser = null
+
+  const currentUser = {
+    id: 1,
+    username: "Anna",
+    isSeller: true,
+  };
+
   return (
-    <nav className={active || pathname !== "/" ? "navbar active" : "navbar"}>
+    <div className={active || pathname !== "/" ? "navbar active" : "navbar"}>
       <div className="container">
         <div className="logo">
-          <Link className="link" to={"/"}>
-            <span className="text">fiverr</span>
+          <Link className="link" to="/">
+            <span className="text">liverr</span>
           </Link>
           <span className="dot">.</span>
         </div>
         <div className="links">
-          <span>Fiverr Bussiness</span>
+          <span>Liverr Business</span>
           <span>Explore</span>
           <span>English</span>
-          <span>Sing in</span>
-          {!currentUser.isSeller && <span>Become a Seller</span>}
-          {currentUser && <button>Join</button>}
-          {currentUser && (
-            <div className="user" onClick={() => setOpen(!open)}>
+          {!currentUser?.isSeller && <span>Become a Seller</span>}
+          {currentUser ? (
+            <div className="user" onClick={()=>setOpen(!open)}>
               <img
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png"
-                alt="logo"
+                src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
               />
               <span>{currentUser?.username}</span>
-
-              {open && (
-                <div className="options">
-                  {currentUser.isSeller && (
-                    <>
-                      <Link className="link" to="/mygigs">
-                        Gigs
-                      </Link>
-                      <Link className="link" to="/add">
-                        Add new Gig
-                      </Link>
-                    </>
-                  )}
-                  <Link className="link" to="/orders">
-                    Order
-                  </Link>
-                  <Link className="link" to="/messages">
-                    Messages
-                  </Link>
-                  <Link className="link" to="/">
-                    Logout
-                  </Link>
-                </div>
-              )}
+              {open && <div className="options">
+                {currentUser.isSeller && (
+                  <>
+                    <Link className="link" to="/mygigs">
+                      Gigs
+                    </Link>
+                    <Link className="link" to="/add">
+                      Add New Gig
+                    </Link>
+                  </>
+                )}
+                <Link className="link" to="/orders">
+                  Orders
+                </Link>
+                <Link className="link" to="/messages">
+                  Messages
+                </Link>
+                <Link className="link" to="/">
+                  Logout
+                </Link>
+              </div>}
             </div>
+          ) : (
+            <>
+              <span>Sign in</span>
+              <Link className="link" to="/register">
+                <button>Join</button>
+              </Link>
+            </>
           )}
         </div>
       </div>
-      {active ||
-        (pathname !== "/" && (
-          <>
-            <hr />
-            <div className="menu">
-              <Link to="/" className="link">
-                Graphics & Desing
-              </Link>
-              <Link to="/" className="link">
-                Video & Animation
-              </Link>
-              <Link to="/" className="link">
-                writing & Translation
-              </Link>
-              <Link to="/" className="link">
-                AI Services
-              </Link>
-              <Link to="/" className="link">
-                Digitasl Marketing
-              </Link>
-              <Link to="/" className="link">
-                Music & Audio
-              </Link>
-              <Link to="/" className="link">
-                Programming & Tech
-              </Link>
-              <Link to="/" className="link">
-                Business
-              </Link>
-              <Link to="/" className="link">
-                Lifestyle
-              </Link>
-            </div>
-          </>
-        ))}
-    </nav>
+      {(active || pathname !== "/") && (
+        <>
+          <hr />
+          <div className="menu">
+            <Link className="link menuLink" to="/">
+              Graphics & Design
+            </Link>
+            <Link className="link menuLink" to="/">
+              Video & Animation
+            </Link>
+            <Link className="link menuLink" to="/">
+              Writing & Translation
+            </Link>
+            <Link className="link menuLink" to="/">
+              AI Services
+            </Link>
+            <Link className="link menuLink" to="/">
+              Digital Marketing
+            </Link>
+            <Link className="link menuLink" to="/">
+              Music & Audio
+            </Link>
+            <Link className="link menuLink" to="/">
+              Programming & Tech
+            </Link>
+            <Link className="link menuLink" to="/">
+              Business
+            </Link>
+            <Link className="link menuLink" to="/">
+              Lifestyle
+            </Link>
+          </div>
+          <hr />
+        </>
+      )}
+    </div>
   );
-};
+}
+
+export default Navbar;
--- src/pages/gig/Gig.tsx
+++ src/pages/gig/Gig.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React from "react";
+import "./Gig.scss";
 import { Slide } from '../../components/slide/Slide';
-import './Gig.scss';
 
-export const Gig = () => {
+export const  Gig =()=> {
   return (
     <div className="gig">
       <div className="container">
         <div className="left">
-          <span className="breadCrums">{'FIVEER > GRAPHICS & DESING >'}</span>
-          <h1> I will create ai generated art for your</h1>
+          <span className="breadcrumbs">Liverr  Graphics & Design </span>
+          <h1>I will create ai generated art for you</h1>
           <div className="user">
-            <img src="" alt=""  className='pp'/>
-            <span>John Dow</span>
-            <div className="starts">
+            <img
+              className="pp"
+              src="https://images.pexels.com/photos/720327/pexels-photo-720327.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <span>Anna Bell</span>
+            <div className="stars">
               <img src="/img/star.png" alt="" />
               <img src="/img/star.png" alt="" />
               <img src="/img/star.png" alt="" />
@@ -21,38 +25,46 @@ export const Gig = () => {
               <span>5</span>
             </div>
           </div>
-          <Slide slidesToShow={1} arrowsScroll={1}>
+          <Slide slidesToShow={1} arrowsScroll={1} className="slider">
             <img
-              src="https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/225619919/original/0932a0b7ee036a5be4b30b772cff57df5e876c7e/create-ai-generated-art-prints.png"
+              src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg?auto=compress&cs=tinysrgb&w=1600"
               alt=""
             />
             <img
-              src="https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs2/225619919/original/3b1f0a9d6c446fb774d1894d7b3f911fbc4f856e/create-ai-generated-art-prints.png"
+              src="https://images.pexels.com/photos/1462935/pexels-photo-1462935.jpeg?auto=compress&cs=tinysrgb&w=1600"
               alt=""
             />
             <img
-              src="https://fiverr-res.cloudinary.com/images/t_smartwm/t_main1,q_auto,f_auto,q_auto,f_auto/attachments/delivery/asset/73283ca0d2bed8134b0493e0d867619d-1681134908/8/create-ai-generated-art-prints.png"
+              src="https://images.pexels.com/photos/1054777/pexels-photo-1054777.jpeg?auto=compress&cs=tinysrgb&w=1600"
               alt=""
             />
           </Slide>
-          <h2>About this gig</h2>
+          <h2>About This Gig</h2>
           <p>
-            I use an AI program to create Lorem ipsum dolor sit amet
-            consectetur, adipisicing elit. Illo, velit pariatur natus laudantium
-            reprehenderit odit at minus vitae, facilis esse architecto dolor
-            inventore porro et tempora sit, sequi magnam doloribus? Lorem ipsum
-            dolor sit amet consectetur adipisicing elit. Non itaque officiis
-            tenetur atque molestias fugiat, voluptas voluptatum fuga est quos
-            ducimus dignissimos aperiam cumque sequi animi autem doloribus
-            beatae recusandae.
+            I use an AI program to create images based on text prompts. This
+            means I can help you to create a vision you have through a textual
+            description of your scene without requiring any reference images.
+            Some things I've found it often excels at are: Character portraits
+            (E.g. a picture to go with your DnD character) Landscapes (E.g.
+            wallpapers, illustrations to compliment a story) Logos (E.g. Esports
+            team, business, profile picture) You can be as vague or as
+            descriptive as you want. Being more vague will allow the AI to be
+            more creative which can sometimes result in some amazing images. You
+            can also be incredibly precise if you have a clear image of what you
+            want in mind. All of the images I create are original and will be
+            found nowhere else. If you have any questions you're more than
+            welcome to send me a message.
           </p>
           <div className="seller">
             <h2>About The Seller</h2>
             <div className="user">
-              <img src="" alt="" />
+              <img
+                src="https://images.pexels.com/photos/720327/pexels-photo-720327.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
               <div className="info">
-                <span>Joah Doe</span>
-                <div className="starts">
+                <span>Anna Bell</span>
+                <div className="stars">
                   <img src="/img/star.png" alt="" />
                   <img src="/img/star.png" alt="" />
                   <img src="/img/star.png" alt="" />
@@ -67,30 +79,31 @@ export const Gig = () => {
               <div className="items">
                 <div className="item">
                   <span className="title">From</span>
-                  <span className="desc">USE</span>
+                  <span className="desc">USA</span>
                 </div>
                 <div className="item">
-                  <span className="title">From</span>
-                  <span className="desc">USE</span>
+                  <span className="title">Member since</span>
+                  <span className="desc">Aug 2022</span>
                 </div>
                 <div className="item">
-                  <span className="title">From</span>
-                  <span className="desc">USE</span>
+                  <span className="title">Avg. response time</span>
+                  <span className="desc">4 hours</span>
                 </div>
                 <div className="item">
-                  <span className="title">From</span>
-                  <span className="desc">USE</span>
+                  <span className="title">Last delivery</span>
+                  <span className="desc">1 day</span>
                 </div>
                 <div className="item">
-                  <span className="title">From</span>
-                  <span className="desc">USE</span>
+                  <span className="title">Languages</span>
+                  <span className="desc">English</span>
                 </div>
               </div>
+              <hr />
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
-                dolorum quis aperiam commodi quae saepe ea pariatur magnam
-                molestias sequi temporibus, quod omnis reiciendis, tempora ullam
-                porro libero, ab quasi!
+                My name is Anna, I enjoy creating AI generated art in my spare
+                time. I have a lot of experience using the AI program and that
+                means I know what to prompt the AI with to get a great and
+                incredibly detailed result.
               </p>
             </div>
           </div>
@@ -98,16 +111,23 @@ export const Gig = () => {
             <h2>Reviews</h2>
             <div className="item">
               <div className="user">
-                <img src="" alt="" />
+                <img
+                  className="pp"
+                  src="https://images.pexels.com/photos/839586/pexels-photo-839586.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                  alt=""
+                />
                 <div className="info">
-                  <span>John Doe</span>
+                  <span>Garner David</span>
                   <div className="country">
-                    <img src="" alt="" />
+                    <img
+                      src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1fa-1f1f8.png"
+                      alt=""
+                    />
                     <span>United States</span>
                   </div>
                 </div>
               </div>
-              <div className="starts">
+              <div className="stars">
                 <img src="/img/star.png" alt="" />
                 <img src="/img/star.png" alt="" />
                 <img src="/img/star.png" alt="" />
@@ -116,24 +136,147 @@ export const Gig = () => {
                 <span>5</span>
               </div>
               <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim,
-                eum accusantium! Minima dolor eveniet veniam culpa architecto
-                tenetur quia ad provident unde sit sequi laudantium,
-                reprehenderit sapiente omnis, nemo magni.
+                I just want to say that art_with_ai was the first, and after
+                this, the only artist Ill be using on Fiverr. Communication was
+                amazing, each and every day he sent me images that I was free to
+                request changes to. They listened, understood, and delivered
+                above and beyond my expectations. I absolutely recommend this
+                gig, and know already that Ill be using it again very very soon
               </p>
               <div className="helpful">
                 <span>Helpful?</span>
                 <img src="/img/like.png" alt="" />
-                <span>YES</span>
+                <span>Yes</span>
                 <img src="/img/dislike.png" alt="" />
-                <span>No?</span>
+                <span>No</span>
               </div>
             </div>
             <hr />
+            <div className="item">
+              <div className="user">
+                <img
+                  className="pp"
+                  src="https://images.pexels.com/photos/4124367/pexels-photo-4124367.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                  alt=""
+                />
+                <div className="info">
+                  <span>Sidney Owen</span>
+                  <div className="country">
+                    <img
+                      src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1e9-1f1ea.png"
+                      alt=""
+                    />
+                    <span>Germany</span>
+                  </div>
+                </div>
+              </div>
+              <div className="stars">
+                <img src="/img/star.png" alt="" />
+                <img src="/img/star.png" alt="" />
+                <img src="/img/star.png" alt="" />
+                <img src="/img/star.png" alt="" />
+                <img src="/img/star.png" alt="" />
+                <span>5</span>
+              </div>
+              <p>
+                The designer took my photo for my book cover to the next level!
+                Professionalism and ease of working with designer along with
+                punctuality is above industry standards!! Whatever your project
+                is, you need this designer!
+              </p>
+              <div className="helpful">
+                <span>Helpful?</span>
+                <img src="/img/like.png" alt="" />
+                <span>Yes</span>
+                <img src="/img/dislike.png" alt="" />
+                <span>No</span>
+              </div>
+            </div>
+            <hr />
+            <div className="item">
+              <div className="user">
+                <img
+                  className="pp"
+                  src="https://images.pexels.com/photos/842980/pexels-photo-842980.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                  alt=""
+                />
+                <div className="info">
+                  <span>Lyle Giles </span>
+                  <div className="country">
+                    <img
+                      src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1fa-1f1f8.png"
+                      alt=""
+                    />
+                    <span>United States</span>
+                  </div>
+                </div>
+              </div>
+              <div className="stars">
+                <img src="/img/star.png" alt="" />
+                <img src="/img/star.png" alt="" />
+                <img src="/img/star.png" alt="" />
+                <img src="/img/star.png" alt="" />
+                <img src="/img/star.png" alt="" />
+                <span>5</span>
+              </div>
+              <p>
+                Amazing work! Communication was
+                amazing, each and every day he sent me images that I was free to
+                request changes to. They listened, understood, and delivered
+                above and beyond my expectations. I absolutely recommend this
+                gig, and know already that Ill be using it again very very soon
+              </p>
+              <div className="helpful">
+                <span>Helpful?</span>
+                <img src="/img/like.png" alt="" />
+                <span>Yes</span>
+                <img src="/img/dislike.png" alt="" />
+                <span>No</span>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <div className="price">
+            <h3>1 AI generated image</h3>
+            <h2>$ 59.99</h2>
+          </div>
+          <p>
+            I will create a unique high quality AI generated image based on a
+            description that you give me
+          </p>
+          <div className="details">
+            <div className="item">
+              <img src="/img/clock.png" alt="" />
+              <span>2 Days Delivery</span>
+            </div>
+            <div className="item">
+              <img src="/img/recycle.png" alt="" />
+              <span>3 Revisions</span>
+            </div>
+          </div>
+          <div className="features">
+            <div className="item">
+              <img src="/img/greencheck.png" alt="" />
+              <span>Prompt writing</span>
+            </div>
+            <div className="item">
+              <img src="/img/greencheck.png" alt="" />
+              <span>Artwork delivery</span>
+            </div>
+            <div className="item">
+              <img src="/img/greencheck.png" alt="" />
+              <span>Image upscaling</span>
+            </div>
+            <div className="item">
+              <img src="/img/greencheck.png" alt="" />
+              <span>Additional design</span>
+            </div>
+          </div>
+          <button>Continue</button>
+        </div>
       </div>
     </div>
   );
-};
+}
+
